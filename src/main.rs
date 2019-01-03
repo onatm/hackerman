ef11d4783c03208dff27a375d87b7cd6b3acb7dd
onatm <onatmercan@gmail.com>
2019-01-03 00:31:39 +0000

    add basic arg reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::process;
+
+struct Config {
+    pub input: String,
+    pub output: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Usage: hackerman input.asm output.hack");
+        }
+        let input = args[1].clone();
+        let output = args[2].clone();
+
+        Ok(Config { input, output })
+    }
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     println!("hackerman...");
 }
